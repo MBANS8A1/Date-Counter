800926b564176190a0fa222c9675e45aa9aeb5ef
MBANS8A1 <seanmbng@aol.com>
2025-10-13 11:30:52 +0100

    I added the buttons, state variables and label text for the date counter as a starting position (including associated css).

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <button className="numButtons">-</button>
+        <p className="labelText">Step: {step}</p>
+        <button className="numButtons">+</button>
+      </div>
+      <div>
+        <button className="numButtons">-</button>
+        <p className="labelText">Count: {count}</p>
+        <button className="numButtons">+</button>
+      </div>
+    </div>
+  );
+}

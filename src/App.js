10ecb764156969f98c2438190ecab41237ea2a2d
MBANS8A1 <seanmbng@aol.com>
2025-10-13 12:24:39 +0100

    I added the last part of date-related code on lines 52-55 in App.js

--- src/App.js
+++ src/App.js
@@ -13,22 +13,47 @@ export default App;
 function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-  const currentDate = new Date().toDateString();
+  const currentDate = new Date();
+
+  function increaseCount() {
+    setCount((c) => c + step);
+  }
+  function decreaseCount() {
+    setCount((c) => c - step);
+  }
+  function increaseStep() {
+    setStep((st) => st + 1);
+  }
+  function decreaseStep() {
+    setStep((st) => st - 1);
+  }
   return (
     <div>
       <div>
-        <button className="numButtons">-</button>
+        <button className="numButtons" onClick={decreaseStep}>
+          -
+        </button>
         <p className="labelText">Step: {step}</p>
-        <button className="numButtons">+</button>
+        <button className="numButtons" onClick={increaseStep}>
+          +
+        </button>
       </div>
       <div>
-        <button className="numButtons">-</button>
+        <button className="numButtons" onClick={decreaseCount}>
+          -
+        </button>
         <p className="labelText">Count: {count}</p>
-        <button className="numButtons">+</button>
+        <button className="numButtons" onClick={increaseCount}>
+          +
+        </button>
       </div>
 
       <p className="outputText">
-        {count} days from today is {currentDate}
+        {count} days from today is &nbsp;
+        {new Date(
+          currentDate.setDate(currentDate.getDate() + count)
+        ).toDateString()}
+        .
       </p>
     </div>
   );

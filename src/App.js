97462c4c8c7eb9a22b0a6c93ebeaeafd7d7c7817
MBANS8A1 <seanmbng@aol.com>
2025-10-19 22:38:42 +0100

    I have used {} JavaScript mode around the div container for the Reset button. With the ternary operator if either the count or step change from their initial states the Reset button will be rendered but if these state variables have not been changed the Reset button will not be visible.

--- src/App.js
+++ src/App.js
@@ -71,9 +71,11 @@ function Counter() {
         </span>
         <span>{currentDate.toDateString()}</span>
       </p>
-      <div>
-        <button onClick={handleReset}>Reset</button>
-      </div>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
